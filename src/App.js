4d2a566eaca899d9baa24ec9835a51d5504f3a9d
Gabriel Marcal <gmarcal6@gmail.com>
2020-11-16 20:51:33 -0300

    Commit para tirar o conteudo representativo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries} from 'react-vis';
+import lejour from './lejour.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <div className="shadow-sm container-fluid">
+    <nav className="navbar navbar-light">
+      <a className="navbar-brand" href="#">
+        <img src={lejour} loading="lazy" alt='logo' />
+      </a>
+      <ul className="navbar-nav mr-right">
+        <li><button type="button" className="btn btn-light rounded-pill btn-sm"><i className="far fa-question-circle"></i>
+            Ajuda</button></li>
+        <li><button type="button" className="btn btn-light rounded-pill btn-sm">Menu</button></li>
+      </ul>
+    </nav>
+  </div>
+
+  <div className="container p-5">
+    <div id="teste1" className="row">
+      
+      
     </div>
-  );
+  </div>
+
+
+
+  <footer className="footer"></footer>
+  
+</div>
+);
+  }
 }
+
 
 export default App;
